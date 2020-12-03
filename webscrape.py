86f45319ad8eaaafa4e158de1c5aa41a399ec7a2
Julian <julianibibo@gmail.com>
2020-12-03 23:18:14 +0100

    testing

---
+++ webscrape.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "http://www.bureauwbtv.nl/tolkvertaler-search/api/search/s"
+
+content = {
+    "searchType": "wbtv_search",
+    "wbtvNumber": 1
+}
+
+headers = {
+}
+
+for i in range(1, 10):
+    try:
+        response = requests.post(url, headers=headers, json=content)
+    except Exception as ex:
+        print(f"Exception on POST for #{i}: {ex}")
+    
+    
+
+print(response.text[:50])
